bceb1ff5fef2f4e6ebbbb1bfc716028c7114a398
ajay-jagdale <ajayjagdale2912@gmail.com>
2024-12-02 22:32:34 +0530

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,13 +36,13 @@ function Header() {
                             <div class="hidden sm:ml-6 sm:block">
                                 <div class="flex space-x-4">
                                     {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                                    <a href="" class="rounded-md px-3 py-2 text-sm" aria-current="page">Microsoft 365</a>
-                                    <a href="" class="rounded-md px-3 py-2 text-sm">Teams</a>
-                                    <a href="" class="rounded-md px-3 py-2 text-sm">Copilot</a>
-                                    <a href="" class="rounded-md px-3 py-2 text-sm">Windows</a>
-                                    <a href="" class="rounded-md px-3 py-2 text-sm">Surface</a>
-                                    <a href="" class="rounded-md px-3 py-2 text-sm">Xbox</a>
-                                    <a href="" class="rounded-md px-3 py-2 text-sm">Support</a>
+                                    <a href="#" class="rounded-md px-3 py-2 text-sm" aria-current="page">Microsoft 365</a>
+                                    <a href="#" class="rounded-md px-3 py-2 text-sm">Teams</a>
+                                    <a href="#" class="rounded-md px-3 py-2 text-sm">Copilot</a>
+                                    <a href="#" class="rounded-md px-3 py-2 text-sm">Windows</a>
+                                    <a href="#" class="rounded-md px-3 py-2 text-sm">Surface</a>
+                                    <a href="#" class="rounded-md px-3 py-2 text-sm">Xbox</a>
+                                    <a href="#" class="rounded-md px-3 py-2 text-sm">Support</a>
                                 </div>
                             </div>
                         </div>
@@ -76,9 +76,9 @@ function Header() {
 
                                 <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                                     {/* Active: "bg-gray-100 outline-none", Not Active: "" */}
-                                    <a href="" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                                    <a href="" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                                    <a href="" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
                                 </div>
                             </div>
                         </div>
@@ -89,10 +89,10 @@ function Header() {
                 <div class="sm:hidden" id="mobile-menu">
                     <div class="space-y-1 px-2 pb-3 pt-2">
                         {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                        <a href="" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
-                        <a href="" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-                        <a href="" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-                        <a href="" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
+                        <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
+                        <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
+                        <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
+                        <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
                     </div>
                 </div>
             </nav>
